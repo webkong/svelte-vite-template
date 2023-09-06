60b110fe498492c62220fdd50e5c8c9759cdef82
webkong <772930508@qq.com>
2023-09-06 11:34:16 +0800

    feat: add analyzer

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import bundleAnalyzer from "rollup-plugin-bundle-analyzer";
+
+// 获取执行时的参数 --report, 用于打包分析
+const npm_lifecycle_script = process.env.npm_lifecycle_script ;
+const isReport = npm_lifecycle_script.indexOf('--report') > -1;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), isReport? bundleAnalyzer(): null],
 
 	build: {
 		rollupOptions: {
